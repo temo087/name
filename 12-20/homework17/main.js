a37b2227e4135c10c97859666ee0c1b817790db2
temo087 <lizii7724@gmail.com>
2023-11-16 01:33:28 +0400

    rame0

---
+++ 12-20/homework17/main.js
@@ -0,0 +1,28 @@
+
+const arrow = document.querySelectorAll('#arrow')
+const baro = document.querySelectorAll('.teqsti')
+const P = document.querySelectorAll('#ptag')
+const box = document.querySelectorAll('#box')
+
+for (let i = 0; i < box.length; i++) {
+    baro[i].addEventListener("click", function() {
+        baro[i].classList.toggle("black");
+        for (let j = 0; j < baro.length; j++) {
+            if (j !== i) {
+                baro[j].classList.remove("black");
+            }
+        }
+        arrow[i].classList.toggle("rotate");
+        for (let j = 0; j < box.length; j++) {
+            if (j !== i) {
+            arrow[j].classList.remove("rotate");
+            }
+        }
+        P[i].classList.toggle("active");
+        for (let j = 0; j < box.length; j++) {
+            if (j !== i) {
+                P[j].classList.remove("active");
+            }
+        }
+    });
+}
